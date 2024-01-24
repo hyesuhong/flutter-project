e8e9d09c97d0cc35f1e9c60b4e9c2ef174cf8072
hyesuhong <honghs95@gmail.com>
2024-01-24 10:39:10 +0900

    feat: add card area

--- lib/main.dart
+++ lib/main.dart
@@ -40,6 +40,7 @@ class MyApp extends StatelessWidget {
                       Text(
                         'Welcome back',
                         style: TextStyle(
+                          fontSize: 12,
                           color: Colors.white.withOpacity(0.7),
                         ),
                       ),
@@ -82,6 +83,103 @@ class MyApp extends StatelessWidget {
                   ),
                 ],
               ),
+              const SizedBox(
+                height: 80,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.baseline,
+                textBaseline: TextBaseline.alphabetic,
+                children: [
+                  Text(
+                    'Wallets',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                  Text(
+                    'View All',
+                    style: TextStyle(
+                      color: Colors.white.withOpacity(0.7),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: const Color(0xff1f2123),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    bottomRight: Radius.circular(20),
+                    topRight: Radius.elliptical(60, 40),
+                    bottomLeft: Radius.elliptical(60, 40),
+                  ),
+                ),
+                padding: EdgeInsets.only(
+                  top: 24,
+                  bottom: 32,
+                  left: 16,
+                  right: 16,
+                ),
+                clipBehavior: Clip.hardEdge,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Euro',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.w500),
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              '6 428',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 8,
+                            ),
+                            Text(
+                              'EUR',
+                              style: TextStyle(
+                                color: Colors.white.withOpacity(0.7),
+                                fontSize: 12,
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                    Transform(
+                      transform: Matrix4.identity()
+                        ..translate(-5.0, 30.0)
+                        ..scale(2.5, 2.5, 2.5),
+                      alignment: Alignment.center,
+                      child: Icon(
+                        Icons.euro,
+                        color: Colors.white,
+                        size: 80,
+                      ),
+                    ),
+                  ],
+                ),
+              )
             ],
           ),
         ),
