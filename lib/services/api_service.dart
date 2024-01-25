50b4ffa88f833e87fb892810c04704ac1f728590
hyesuhong <honghs95@gmail.com>
2024-01-25 14:32:24 +0900

    feat: add api class & model about webtoon, before fetching data

---
+++ lib/services/api_service.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+
+import 'package:flutter_application_1/models/toon_model.dart';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  final String baseURL = 'https://webtoon-crawler.nomadcoders.workers.dev';
+
+  final String today = '/today';
+
+  Future<List<ToonModel>> getTodayToons() async {
+    final url = Uri.parse('$baseURL$today');
+    final response = await http.get(url);
+
+    if (response.statusCode != 200) {
+      throw Error();
+    }
+
+    final List<dynamic> toons = jsonDecode(response.body);
+    var toonData = toons.map((e) => ToonModel.fromJson(e)).toList();
+
+    return toonData;
+  }
+}
