7c54e7e1551fa46df6ded81314656499898ef9ce
hyesuhong <honghs95@gmail.com>
2024-01-25 17:00:25 +0900

    feat: divide widget(appBar, toonCard)

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/models/toon_model.dart';
 import 'package:flutter_application_1/services/api_service.dart';
-import 'package:flutter_application_1/styles/font.dart';
+import 'package:flutter_application_1/widgets/header_bar.dart';
+import 'package:flutter_application_1/widgets/toon_card.dart';
 
 class Home extends StatelessWidget {
   Home({super.key});
@@ -12,20 +13,7 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        surfaceTintColor: Colors.white,
-        shadowColor: Colors.black12,
-        backgroundColor: Colors.white,
-        foregroundColor: Colors.grey[600],
-        elevation: 5,
-        title: Text(
-          'Today\'s toons',
-          style: TextStyle(
-            fontSize: 20,
-            fontVariations: [NotoSansKRWeight.w500],
-          ),
-        ),
-      ),
+      appBar: HeaderBar(title: 'Today\'s toons'),
       body: FutureBuilder(
         future: toons,
         builder: (context, snapshot) {
@@ -57,39 +45,7 @@ class Home extends StatelessWidget {
       separatorBuilder: (context, index) => SizedBox(width: 40),
       itemBuilder: (context, index) {
         var toon = snapshot.data![index];
-        return Column(
-          children: [
-            Container(
-              width: 250,
-              clipBehavior: Clip.hardEdge,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-                boxShadow: [
-                  BoxShadow(
-                    blurRadius: 10,
-                    offset: Offset(0, 8),
-                    color: Colors.black.withOpacity(0.3),
-                  )
-                ],
-              ),
-              child: Image.network(
-                toon.thumb,
-                headers: const {
-                  "User-Agent":
-                      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-                },
-              ),
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            Text(
-              toon.title,
-              style: TextStyle(
-                  fontSize: 16, fontVariations: [NotoSansKRWeight.w400]),
-            )
-          ],
-        );
+        return ToonCard(toon: toon);
       },
     );
   }
