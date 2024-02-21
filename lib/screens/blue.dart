73eb2b0023f3664f6745364ee203941de4ecf996
hyesuhong <honghs95@gmail.com>
2024-02-21 13:38:42 +0900

    feat: add connection function

--- lib/screens/blue.dart
+++ lib/screens/blue.dart
@@ -13,7 +13,25 @@ class BluetoothPage extends StatefulWidget {
 
 class _BluetoothPageState extends State<BluetoothPage> {
   BluetoothAdapterState _adapterState = BluetoothAdapterState.unknown;
+  List<ScanResult> _deviceList = [];
+  BluetoothDevice? _currentDevice;
+
   late StreamSubscription<BluetoothAdapterState> _adapterStateSubscription;
+
+  void _initBluetooth() async {
+    bool isSupported = await FlutterBluePlus.isSupported;
+
+    if (!isSupported) {
+      print('Bluetooth not supported by this device');
+      return;
+    }
+
+    if (mounted) {
+      setState(() {
+        _adapterStateSubscription = _getBluetoothAdapterSubscription();
+      });
+    }
+  }
 
   StreamSubscription<BluetoothAdapterState> _getBluetoothAdapterSubscription() {
     return FlutterBluePlus.adapterState.listen(
@@ -42,11 +60,13 @@ class _BluetoothPageState extends State<BluetoothPage> {
     final StreamSubscription<List<ScanResult>> scanSubscription =
         FlutterBluePlus.onScanResults.listen(
       (results) {
-        // print(results);
         if (results.isNotEmpty) {
-          ScanResult r = results.last; // the most recently found device
-          print(
-              '${r.device.remoteId}: "${r.advertisementData.advName}" found!');
+          ScanResult res = results.last;
+          // print(res);
+
+          setState(() {
+            _deviceList.add(res);
+          });
         }
       },
       onError: (error) {
@@ -68,22 +88,33 @@ class _BluetoothPageState extends State<BluetoothPage> {
     });
   }
 
+  void _connectDevice(BluetoothDevice device) async {
+    print(device);
+
+    StreamSubscription<BluetoothConnectionState> subscription =
+        device.connectionState.listen((BluetoothConnectionState state) {
+      print(state);
+      if (state == BluetoothConnectionState.disconnected) {
+        print(device.disconnectReason);
+      } else if (state == BluetoothConnectionState.connected) {
+        print(device);
+      }
+    });
+
+    device.cancelWhenDisconnected(subscription, delayed: true, next: true);
+
+    await device.connect();
+
+    await device.disconnect();
+
+    subscription.cancel();
+  }
+
   @override
-  void initState() async {
+  void initState() {
     super.initState();
 
-    bool isSupported = await FlutterBluePlus.isSupported;
-
-    if (!isSupported) {
-      print('Bluetooth not supported by this device');
-      return;
-    }
-
-    if (mounted) {
-      setState(() {
-        _adapterStateSubscription = _getBluetoothAdapterSubscription();
-      });
-    }
+    _initBluetooth();
   }
 
   @override
@@ -95,13 +126,53 @@ class _BluetoothPageState extends State<BluetoothPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Bluetooth State'),
-            Text(_adapterState.name),
-          ],
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('Bluetooth State'),
+                Text(_adapterState.name),
+                ListView.separated(
+                  physics: NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  itemCount: _deviceList.length,
+                  padding: EdgeInsets.all(8),
+                  separatorBuilder: (context, index) {
+                    return const Divider();
+                  },
+                  itemBuilder: (context, index) {
+                    ScanResult res = _deviceList[index];
+                    return Column(
+                      children: [
+                        Text(
+                          res.device.remoteId.toString(),
+                          style: TextStyle(
+                            color: Colors.grey,
+                            fontSize: 14,
+                          ),
+                        ),
+                        Text(
+                          res.advertisementData.advName,
+                          style: TextStyle(
+                            fontSize: 18,
+                          ),
+                        ),
+                        if (res.advertisementData.connectable)
+                          TextButton(
+                            onPressed: () {
+                              _connectDevice(res.device);
+                            },
+                            child: Text('Connect'),
+                          ),
+                      ],
+                    );
+                  },
+                )
+              ],
+            ),
+          ),
         ),
       ),
       bottomNavigationBar: BottomNavigation(index: 2),
